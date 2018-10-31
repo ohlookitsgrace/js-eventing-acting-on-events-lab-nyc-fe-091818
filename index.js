bc36b99aa039954dfa042ce1af59c08755a036b5
Grace Han <ohlookitsgrace@gmail.com>
2018-10-31 00:45:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+const dodger = document.getElementById('dodger')
+
+//dodger.style.bottom = '380px'
+
+function moveDodger(event) {
+  const keycode = event.which
+  console.log(keycode)
+  if (keycode === 37) {
+    //this is left arrow key
+    let oldLeft = dodger.style.left
+    
+    if (oldLeft === '0px') {
+      return
+    }
+    
+    console.log(oldLeft)
+    oldLeft = parseInt(oldLeft, 10)
+    
+    dodger.style.left = (oldLeft - 10) + 'px'
+    } else if (keycode === 39) {
+    moveDodgerRight(event)
+  }
+    //figure out where it is
+}
+
+function moveDodgerRight(event) {
+  if (event.which === 39) {
+    let oldLocation = dodger.style.left
+    
+    if (oldLocation === '360px') {
+      return 
+    }
+    
+    oldLocation  = parseInt(oldLocation, 10)
+    
+    dodger.style.left = (oldLocation + 10) +'px'
+  }
+}
+
+function moveDodgerRight () {}
+
+document.addEventListener('keydown', moveDodger)
